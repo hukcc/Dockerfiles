c418fcba044f7ec4e3526128ecf8858c0db21f1b
hukcc <849208770@qq.com>
2018-12-13 21:38:36 +0800

    完整的RM开发环境

---
+++ ToRunImages/opencv_cpu.sh
@@ -0,0 +1,6 @@
+#！/bin/bash
+
+xhost +
+
+
+docker run -itv /home/hukcc/floder_to_images:/home --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE --user="mynewuser" hukcc/opencv_cpu /bin/bash
