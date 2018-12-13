1e4dd2bf9881d69d7c2f4d675fddf04365262e16
hukcc <849208770@qq.com>
2018-12-14 00:55:32 +0800

    perfect

--- ToRunImages/opencv_cpu.sh
+++ ToRunImages/opencv_cpu.sh
@@ -3,4 +3,4 @@
 xhost +
 
 
-docker run -itv /home/hukcc/floder_to_images:/home --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE --user="mynewuser" hukcc/opencv_cpu /bin/bash
+docker run -itv /home/hukcc/floder_to_images:/home --device=/dev/video1 --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE --user="root"  hukcc/opencv_cpu /bin/bash
