41cba35d478314889ab7ab07b1a45ccf4173889e
hukcc <849208770@qq.com>
2018-12-18 22:21:39 +0800

    add hexo

---
+++ ToRunImages/hexo.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker run -itv /home/hukcc/floder_to_images/hexo:/hukcc_blog --user="root" hukcc_blog/hexo /bin/bash
--- ToRunImages/opencv_cpu.sh
+++ ToRunImages/opencv_cpu.sh
@@ -1,6 +1,7 @@
 #！/bin/bash
 
 xhost +
+#docker run 可选项 开启外界摄像头以及串口
+#--device=/dev/video1
 
-
-docker run -itv /home/hukcc/floder_to_images:/home --device=/dev/video1 --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE --user="root"  hukcc/opencv_cpu /bin/bash
+docker run -itv /home/hukcc/floder_to_images/opencv_cpu:/RMcodes  --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE --user="root"  hukcc/opencv_cpu /bin/bash
